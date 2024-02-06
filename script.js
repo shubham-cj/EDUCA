8a6a0ec1ab55c3232858d4c9d8bc5575edb44557
shubham-cj <119930498+shubham-cj@users.noreply.github.com>
2024-02-07 00:32:54 +0530

    Home Page Complete

--- script.js
+++ script.js
@@ -3,3 +3,41 @@
 window.addEventListener('scroll', () => {
     document.querySelector("nav").classList.toggle("window-scroll", window.scrollY > 0)
 });
+
+// Show/hide FAQs answers
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("open");
+
+        // change Icon
+        const icon = faq.querySelector(".faq__icon i");
+        if (icon.className === "uil uil-plus") {
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = "uil uil-plus";
+        }
+    });
+});
+
+// Show/Hide Nav menu
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#openMenuBtn");
+const closeBtn = document.querySelector("#closeMenuBtn");
+
+menuBtn.addEventListener("click", () => {
+    menu.style.display = "block";
+    menuBtn.style.display = "none";
+    closeBtn.style.display = "block";
+})
+closeBtn.addEventListener("click", () => {
+    menu.style.display = "none";
+    menuBtn.style.display = "block";
+    closeBtn.style.display = "none";
+})
+window.onscroll = () => {
+    menu.style.display = "none";
+    menuBtn.style.display = "block";
+    closeBtn.style.display = "none";
+}
